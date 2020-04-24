bd81d78ca3cc8c64228c9531cc3f07290abd4321
daovanduc <duc07092000@gmail.com>
2020-04-24 17:42:53 +0700

    update index

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
 <?php
 
+include_once 'action.php';
+
+$numbers = [];
+$value = 22;
+
+for ($i = 0; $i < 100; $i++) {
+    $numbers[$i] = rand(1, 100);
+}
+
+foreach ($numbers as $number) {
+    echo $number . ' ';
+}
+
+$countNumber = countNumber($numbers, $value);
+echo '<br>' . 'The number of occurrences of value = 22 is' . '<br>' . $countNumber;
